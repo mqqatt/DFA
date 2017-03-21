32d55ad2049c61a3d8dd570a48187599b4006fe9
mqqatt <mqqatt@gmail.com>
2017-03-21 22:09:30 +0100

    Create view prototype

--- src/main/java/org/ms/dfa/Main.java
+++ src/main/java/org/ms/dfa/Main.java
@@ -2,20 +2,46 @@ package org.ms.dfa;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private Stage primaryStage;
+    private AnchorPane mainLayout;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+    public void start(Stage primaryStage){
+
+        this.primaryStage = primaryStage;
         primaryStage.setTitle("Desktop Financial Assistant");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        showDFA();
     }
 
+
+    public void showDFA() {
+
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("main.fxml"));
+            mainLayout =  loader.load();
+            Scene scene = new Scene (mainLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
     public static void main(String[] args) {
         launch(args);
